46c3c06d7ea8eb1ebe741a76d83a3463556b0691
Eric Fode <ericfode@gmail.com>
2012-08-10 11:24:55 -0400

    Added some information for how to continue devlopement

--- GraphManager.go
+++ GraphManager.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 //TO-DO:
 //  + make type keys and type set
@@ -8,8 +8,7 @@ package main
 //      + deregister should only work if no nodes of that type exist
 //      + register should only work if type does not already exist
 
-
-import(
+import (
 	"github.com/hoisie/redis.go"
 )
 
@@ -17,90 +16,91 @@ import(
 
 var db redis.Client
 
-type GraphManager struct{
-	nodes map[int] Node*
-	edges map[int] Edge*	
+type GraphManager struct {
+	nodes map[int]*Node
+	edges map[int]*Edge
 }
 
-func ( gm GraphManager* ) Initialize() {
-	db.Set( "nodeCount", 0 )
+func (gm *GraphManager) Initialize() {
+	db.Set("nodeCount", 0)
 }
 
-func ( gm GraphManager* ) Connect( port int, ipAddr String, dbNum int) {
-	db.Addr = ipAddr + ":" + port	
+func (gm *GraphManager) Connect(port int, ipAddr String, dbNum int) {
+	db.Addr = ipAddr + ":" + port
 	db.Db = dbNum
 }
 
 // NODE MANAGEMENT
 
-func ( gm GraphManager* ) CreateNode() Node* {
-	
+func (gm *GraphManager) AddNode(n *Node) *Node {
+
 	//Database
-	index := db.Get( "nodeCount" )
-	db.Sadd( "node:" + index , "" )
-	db.Incr( "nodeCount" )
+
+	index := db.Get("nodeCount")
+	db.Incr("nodeCount")
+	db.Hset("node:"+index, "index", []byte(index))
 	
+	//Add node to index
+	db.Sadd("nodes",[]byte(index))
 	//Local
-	node := new Node()
-	node.id = index
-	gm.nodes[index] = node
-	return node
+	n.id = index
+	gm.nodes[index] = n
+	return n
 }
 
-func ( gm GraphManager* ) DeleteNode( n Node* ) {
+func (gm *GraphManager) DeleteNode(n *Node) {
 	//Database
-	db.Del( n.id )
-
+	db.SRem("nodes",n.id)
 	//Local
 	gm.nodes[n.id] = nil
 	n = nil
 }
 
-func ( gm GraphManager* ) FindNode( name String ) Node* {
 
-
-}
-
-func ( gm GraphManger* ) UpdateNode( n Node* ) Bool {
+func (gm *GraphManager) FindNode(name String) *Node {
 
 }
 
-func ( gm GraphManger* ) GetNode( name String ) Node* {
+func (gm *GraphManger) UpdateNode(n *Node) Bool {
 
 }
 
-func ( gm GraphManager* ) GetAdjPairs( node Node* ) AdjPair[]* {
+func (gm *GraphManger) GetNode(Id String) *Node {
+	db.Get("node:"+index)
+}
+
+func (gm *GraphManager) GetAdjPairs(node *Node) *[]AdjPair {
 
 }
 
 //Add neigbhbor both locally and in db
-func ( gm GraphManager* ) AddNeighbor( node Node*, edge Edge* ) {
+func (gm *GraphManager) AddNeighbor(node *Node, edge *Edge) {
 
 }
 
 //EDGE MANAGEMENT
 
-func ( gm GraphManager* ) CreateEdge() Edge* {
+func (gm *GraphManager) CreateEdge() *Edge {
 
 }
 
-func ( gm GraphManager* ) DeleteEdge( e Edge* ) {
+func (gm *GraphManager) DeleteEdge(e *Edge) {
 
 }
 
-func ( gm GraphManager* ) FindEdge( id int ) Edge* {
+func (gm *GraphManager) FindEdge(id int) *Edge {
 
 }
 
-func ( gm GraphManger* ) UpdateEdge( e Edge* ) Bool {
+func (gm *GraphManger) UpdateEdge(e *Edge) Bool {
 
 }
 
-func ( gm GraphManger* ) GetEdge( id int ) Edge* {
+func (gm *GraphManger) GetEdge(id int) *Edge {
 
 }
 
 //PRIVATE FUNCTIONS
 
-//func ( gm GraphManager* ) addAdjEntry( )
-//func ( gm GraphManager* ) removeAdjEntry( )
+//func ( gm *GraphManager ) addAdjEntry( )
+//func ( gm *GraphManager ) removeAdjEntry( )
