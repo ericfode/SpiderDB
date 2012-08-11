292f3c5d907af7436aea9dae9435f1e54e27b561
Sarah Gonzalez <smar.gonz@gmail.com>
2012-08-11 12:47:47 -0400

    gm - wip.

--- GraphManager.go
+++ GraphManager.go
@@ -12,10 +12,15 @@ package spiderDB
 
 import "github.com/alphazero/Go-Redis"
 
-const currIndex "currIndex"
+// consts (typo prevention)
+const currIndex_s = "currIndex"
+const node_s = "node:"
+const edge_s = "edge:"
+const nodes_s = "nodes"
+const edges_s = "edges"
+const props_s = "props"
 
 type KeyNotFoundError string
-
 
 //Initializes database at startup
 
@@ -28,9 +33,9 @@ type GraphManager struct {
 func (gm *GraphManager) Initialize() {
 	//var e error
 	gm.client, _ = redis.NewSynchClient()
-	gm.client.Set("currIndex", []byte("0"))
+	gm.client.Set(currIndex_s, []byte("0"))
 
-	gm.Connect(port, ipAddr, dbNum)	
+	gm.Connect(port, ipAddr, dbNum)
 
 	gm.nodes = make(map[string]*Node)
 	gm.edges = make(map[string]*Edge)
@@ -41,8 +46,8 @@ func (gm *GraphManager) Connect(port int, ipAddr string, dbNum int) {
 }
 
 func (gm *GraphManager) GetNextIndex() string {
-	index, _ := gm.client.Get("currIndex")
-	gm.client.Incr("currIndex")
+	index, _ := gm.client.Get(currIndex_s)
+	gm.client.Incr(currIndex_s)
 
 	return string(index)
 }
@@ -52,7 +57,7 @@ func (gm *GraphManager) GetNextIndex() string {
 func (gm *GraphManager) AddNode(n *Node) {
 	//Database
 	index := gm.GetNextIndex()
-	nindex := "node:" + index
+	nindex := node_s + index
 	props := n.GetPropMap()
 
 	//Add node props to database node
@@ -61,37 +66,38 @@ func (gm *GraphManager) AddNode(n *Node) {
 	}
 
 	//Add node to node-index
-	gm.client.Sadd("nodes", []byte(index))
+	gm.client.Sadd(nodes_s, []byte(nindex))
 
 	//Local
-	n.SetID(index)
-	gm.nodes[index] = n
+	n.SetID(nindex)
+	gm.nodes[nindex] = n
 }
 
 func (gm *GraphManager) DeleteNode(n *Node) {
 	//Database
-	index = n.GetID()
-	gm.client.Srem("nodes", []byte(index))
-	gm.client.Del("node:" + index)
+	nindex = n.GetID()
+	gm.client.Srem(nodes_s, []byte(index))
+	gm.client.Del(nindex)
 	//Local
-	gm.nodes[index] = nil
+	gm.nodes[nindex] = nil
 	n = nil
 }
 
-func (gm *GraphManager) FindNode(index string) *Node {
-	n, ok := gm.nodes[index]
+func (gm *GraphManager) FindNode(nindex string) *Node {
+	n, ok := gm.nodes[nindex]
 	//if local
 	if ok == true {
 		return n
 	}
 	//otherwise, get from db
-	return gm.GetNode(index)
+	return gm.GetNode(nindex)
 }
 
 //bulk update - pushes everything to database
 func (gm *GraphManager) UpdateNode(n *Node) Error {
+
 	
-	e := gm.client.Hset(n.GetID(), "props", n.GetPropMap())
+	e := gm.client.Hset(n.GetID(), props_s, n.GetPropMap())
 	return true
 }
 
@@ -101,7 +107,7 @@ func (gm *GraphManger) UpdateNodeProp(n *Node, prop String, value []byte) {
 }
 
 func (gm *GraphManager) GetNode(index string) *Node {
-	nodeIdx, err := gm.client.Hget("node:"+index, "props")
+	nodeIdx, err := gm.client.Hget(node_s+index, props_s)
 
 	if err != nil || nodeIdx == nil {
 		return nil
@@ -129,18 +135,34 @@ func (gm *GraphManager) GetNeighbors(node *Node) []*Node {
 func (gm *GraphManager) AddEdge(e *Edge) {
 
 	index := gm.GetNextIndex()
-	props := e.GetWeight()
-	gm.client.Hset("edge:"+index, "props", []byte(props))
+	eindex := edge_s+index
+	//props := e.GetPropMap()
+	weight := e.GetWeight()
+
+	gm.client.Hset(eindex, "weight", []byte(weight))
 
 	//Add node to index
-	gm.client.Sadd("edges", []byte(props))
+	gm.client.Sadd(edges_s, []byte(eindex))
 
 	//Local
-	e.SetId(index)
-	gm.edges[index] = e
+	e.SetId(eindex)
+	gm.edges[eindex] = e
 }
 
 func (gm *GraphManager) DeleteEdge(e *Edge) {
+
+	eindex = e.GetId()
+
+	//remove locally
+	gm.edges[ eindex ] = nil
+
+	//remove from database
+	gm.client.Del( eindex )
+
+	//remove from database's edge-index
+	gm.client.Srem(edges_s, eindex)
+
+	e = nil
 }
 
 func (gm *GraphManager) FindEdge(id int) *Edge {
@@ -167,7 +189,7 @@ func (gm *GraphManager) GetNodeEdges(n *Node) map[string][]*Edge {
 }
 
 func (gm *GraphManager) ClearAll() {
-	gm.client.Set("currIndex", []byte("0"))
+	gm.client.Set(currIndex_s, []byte("0"))
 	gm.client.Flushdb()
 	gm.nodes = nil
 	gm.edges = nil
